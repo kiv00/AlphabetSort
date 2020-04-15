529438160d87e27b3e69177c646596a962a7eeb7
kiv00 <vekmnbrkeynbrr@mail.ru>
2020-04-15 14:26:34 +0700

    requested the file name

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
+
+
+
+int main(){
+	setlocale(LC_ALL,"Russian");
+	
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}	
