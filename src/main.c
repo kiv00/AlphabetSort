24e91e9b13fd2b339b64eb0a7b2636a08eb8d582
kiv00 <vekmnbrkeynbrr@mail.ru>
2020-04-15 14:10:01 +0700

    include all libraries

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
+
+
+
+int main(){
+	setlocale(LC_ALL,"Russian");
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}	
