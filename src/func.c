b5017fcd65c9dea5dcf42d6821be84cad648cc94
unknown <egorguslov@mail.ru>
2020-04-19 19:00:46 +0700

    Created a file for functions

---
+++ src/func.c
@@ -0,0 +1,12 @@
+#include "func.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
+
+int Unique(char **S, int s){}
+
+void Sort(char **A, int size, int M){}
