1cced900176342bb8265ef49050e1ce5899a8ff0
unknown <egorguslov@mail.ru>
2020-04-19 19:06:44 +0700

    Sort function added

--- src/func.c
+++ src/func.c
@@ -7,6 +7,20 @@
 #include <conio.h>
 #include <locale.h>
 
-int Unique(char **S, int s){}
+int Unique(char **S, int s){
 
-void Sort(char **A, int size, int M){}
+}
+
+void Sort(char **A, int size, int M){
+char temp[M];
+		for (int n = 1; n < size; n++)
+			for (int m = 0; m < size - n; m++) {
+				int code = strcmp(A[m + 1], A[m]);
+				if (code < 0)
+				{
+					strcpy(temp,A[m]); 
+					strcpy(A[m],A[m+1]);
+					strcpy(A[m+1],temp); 
+				}
+			}
+}
