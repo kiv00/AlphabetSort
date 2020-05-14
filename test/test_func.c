d2b77f152297c7526988b6c6406054c4c6e045f6
kiv00 <kiv00wot@mail.ru>
2020-05-14 13:18:33 +0700

    change .travis

--- test/test_func.c
+++ test/test_func.c
@@ -1,8 +1,11 @@
-си #include "../src/func.h"
+#include "../src/func.h"
+#include <conio.h>
+#include <locale.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "../thirdparty/ctest.h"
 
